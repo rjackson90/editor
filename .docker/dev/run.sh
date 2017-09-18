22e42457d5084cc68acb6a403e393a77fac4cf8e
Richard Jackson <rjackson90@gmail.com>
2017-09-18 01:32:20 +0000

    Create separate run and build scripts for Docker dev server image

--- .docker/dev/run.sh
+++ .docker/dev/run.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# Figure out where this script is located
-PROJECT_DIR=$(dirname "$(readlink -f "$0")")
+# Figure out where this script is located, get project name
+PROJECT_DIR=$(realpath "$(dirname "$(readlink -f "$0")")/../..")
+PROJECT_NAME=$(basename "$PROJECT_DIR")
 
 # Replace the current shell with an interactive Docker container
-exec docker run -it \
+exec docker run -it --rm \
 	-v /etc/group:/etc/group:ro \
 	-v /etc/passwd:/etc/passwd:ro \
 	-u $(id -u $USER):$(id -g $USER) \
 	-v $PROJECT_DIR:/app \
 	-p 80:8080 \
-	devbox
+	"${PROJECT_NAME}-dev"
